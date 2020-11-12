2d0c1356d9570bab67efa61a8a36106ffcf84683
Andrew Reid <areid.sj@gmail.com>
2020-11-12 15:52:38 -0700

    Add validations for cafe model

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,2 +1,6 @@
 class Cafe < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :post_code, presence: true
+  validates :chairs, presence: true
 end
--- spec/models/cafe_spec.rb
+++ spec/models/cafe_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Cafe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:address)}
+    it {should validate_presence_of(:post_code)}
+    it {should validate_presence_of(:chairs)}
+  end
 end
