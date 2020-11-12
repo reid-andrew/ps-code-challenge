4b0ad16f2d817264a671d3b669507770edba0248
Andrew Reid <areid.sj@gmail.com>
2020-11-12 16:31:11 -0700

    Set category for LS1 post_codes

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -3,4 +3,31 @@ class Cafe < ApplicationRecord
   validates :address, presence: true
   validates :post_code, presence: true
   validates :chairs, presence: true
+
+  before_create :set_category
+
+  private
+
+  def set_category
+    category = case post_code[0..2]
+    when "LS1"
+      set_ls1_code
+    when "LS2"
+      set_ls2_code
+    else
+      'other'
+    end
+    self.category = category
+  end
+
+  def set_ls1_code
+    case chairs
+    when 0..10
+      'ls1 small'
+    when 11..100
+      'ls1 medium'
+    else
+      'ls1 large'
+    end
+  end
 end
--- spec/models/cafe_spec.rb
+++ spec/models/cafe_spec.rb
@@ -7,4 +7,37 @@ RSpec.describe Cafe, type: :model do
     it {should validate_presence_of(:post_code)}
     it {should validate_presence_of(:chairs)}
   end
+
+  describe 'model methods' do
+    describe 'set_category' do
+      describe 'sets correct category for LS1 prefixes' do
+        before(:each) do
+          @ls1_cafe_small = {name: Faker::Restaurant.name,
+            address: Faker::Address.street_address,
+            post_code: "LS1 #{Faker::Number.number(digits: 1)}#{Faker::Alphanumeric.alpha(number: 2)}",
+            chairs: Faker::Number.number(digits: 1)}
+          @ls1_cafe_medium = {name: Faker::Restaurant.name,
+            address: Faker::Address.street_address,
+            post_code: "LS1 #{Faker::Number.number(digits: 1)}#{Faker::Alphanumeric.alpha(number: 2)}",
+            chairs: Faker::Number.number(digits: 2)}
+          @ls1_cafe_large = {name: Faker::Restaurant.name,
+            address: Faker::Address.street_address,
+            post_code: "LS1 #{Faker::Number.number(digits: 1)}#{Faker::Alphanumeric.alpha(number: 2)}",
+            chairs: Faker::Number.number(digits: 3)}
+        end
+        it 'sets small category' do
+          cafe = Cafe.create!(@ls1_cafe_small)
+          expect(cafe.category).to eq("ls1 small")
+        end
+        it 'sets medium category' do
+          cafe = Cafe.create!(@ls1_cafe_medium)
+          expect(cafe.category).to eq("ls1 medium")
+        end
+        it 'sets large category' do
+          cafe = Cafe.create!(@ls1_cafe_large)
+          expect(cafe.category).to eq("ls1 large")
+        end
+      end
+    end
+  end
 end
