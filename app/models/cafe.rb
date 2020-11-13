15c18565b55bbe5517e76dfa42812ba78685a1f1
Andrew Reid <areid.sj@gmail.com>
2020-11-12 17:05:19 -0700

    Add set LS2 category method

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -4,23 +4,23 @@ class Cafe < ApplicationRecord
   validates :post_code, presence: true
   validates :chairs, presence: true
 
-  before_create :set_category
+  before_save :set_category
 
   private
 
   def set_category
     category = case post_code[0..2]
     when "LS1"
-      set_ls1_code
+      set_ls1_category
     when "LS2"
-      set_ls2_code
+      set_ls2_category
     else
       'other'
     end
     self.category = category
   end
 
-  def set_ls1_code
+  def set_ls1_category
     case chairs
     when 0..10
       'ls1 small'
@@ -29,5 +29,20 @@ class Cafe < ApplicationRecord
     else
       'ls1 large'
     end
+  end
+
+  def set_ls2_category
+    cafes = Cafe.where("cafes.post_code LIKE 'LS2%'").map { |x| x.chairs }.sort
+    count = cafes.count
+    return 'ls2 large' if count <= 1
+
+    median = if count % 2 == 0
+      first_half = (cafes[0...(count/2)])
+      second_half = (cafes[(count/2)..-1])
+      (first_half[-1] + second_half[0]).to_f / 2.to_f
+    else
+      cafes[(count/2).floor]
+    end
+    self.chairs < median ? 'ls2 small' : 'ls2 large'
   end
 end
